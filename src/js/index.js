154d7d8c361c1321aab8074818bd8518b4bc7d89
Zack Stickles <zstickles@gmail.com>
2019-08-24 18:03:48 -0500

    Updating colors

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,19 @@
 import '../scss/main.scss';
 
-console.log('Hello, styles');
+const $controls = document.querySelector('.controls');
+
+const updateColorElements = (color = 'black') => {
+  const $colorElements = document.querySelectorAll('.js-colors');
+  const colorClass = color === 'black'
+    ? 'black-on-white'
+    : 'white-on-black';
+
+  $colorElements.forEach((el) => {
+    console.log('el', el);
+    el.classList.remove('black-on-white');
+    el.classList.remove('white-on-black');
+    el.classList.add(colorClass);
+  });
+}
+
+updateColorElements();
